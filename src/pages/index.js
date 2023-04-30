e288904435713774fe5587d5e9c9b23374197e08
BenjaminHerrera <BenHerrera1044@outlook.com>
2023-04-29 21:09:49 -0700

    minor changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -224,15 +224,13 @@ export default function Home() {
                   <div className="md:w-full">
                     <textarea
                       className="bg-gray-200 font-santello appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-300"
-                      id="inline-full-name"
+                      id="prompt"
                       name="name"
                       rows="3" 
                       style={{ resize: 'vertical' }} 
                     ></textarea>
                   </div>
                 </div>
-
-
                 <div className="flex justify-center ">
                   {inputButton}
                 </div>
